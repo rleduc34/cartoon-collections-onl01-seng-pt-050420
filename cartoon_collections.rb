1cd36cb2c78e2337f1dad18efd53b00e999b2a36
Robert Leduc <rleduc34@gmail.com>
2020-05-18 03:07:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,33 +1,36 @@
-def roll_call_dwarves(array)
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, index|
+    index += 1
+    puts "#{index}. #{name}"
+  end
+end
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.capitalize << "!"
+  end
+end
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+  end
+end
+def find_the_cheese(array)
+  # the array below is here to help
   i = 0
-  while i < array.length
-    puts "#{i + 1}. #{array[i]}"
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  while i < cheese_types.length
+    return cheese_types[i] if array.include?(cheese_types[i])
     i += 1
   end
 end
 
-def summon_captain_planet(array)# code an argument here
-  new_array = []
-  i = 0
-  while i < array.length
-    new_array << array[i].capitalize + "!"
-  end
-  new_array
-end
 
-def long_planeteer_calls(array)
-  i = 0
-  if  array.any? {|i| i.length > 4}
-    return true
-  else
-    return false
-  i = i + 1
-  end
-end
 
-def find_the_cheese
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do |type|
-    cheese_types.include?(type)
-  end
-end
+
+
+
+
+
+
+
+
